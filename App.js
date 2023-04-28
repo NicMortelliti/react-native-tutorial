e1e0a9decaaeb63763ea3cfcb9fa948b20f956d1
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-28 11:15:08 -0700

    Tutorialspoint tutorial outdated. Reset app to new to-do tutorial

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
 import React from "react";
 import { View } from "react-native";
-import PresentationalComponent from "./PresentationalComponent";
 
 export default class App extends React.Component {
-  state = {
-    myState: "This is my state",
-  };
-
-  updateState = () => {
-    this.setState({ myState: "The state is updated" });
-  };
-
   render() {
-    return (
-      <View>
-        <PresentationalComponent
-          myState={this.state.myState}
-          updateState={this.updateState}
-        />
-      </View>
-    );
+    return <View></View>;
   }
 }
