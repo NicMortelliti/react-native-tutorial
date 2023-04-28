c2ef1e8144d86cc39191ce005ced8ec214650a52
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-28 11:29:07 -0700

    add image component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import Header from "./src/components/Header";
+import DisplayImage from "./src/components/DisplayImage";
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
         <Header />
+        <DisplayImage taskStatus="0" />
       </View>
     );
   }
